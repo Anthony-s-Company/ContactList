ce6dd25b13c2f796a44c0109c535d44bfdd63e7b
Anthony Ruiz <yobakaj@gmail.com>
2023-07-24 16:04:55 -0400

    fetching data from api

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+import ContactRow from "./ContactRow";
+
+
+const dummyContacts = [
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
+export default function ContactList() {
+  const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setContacts(data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <table>
+      {isLoading ? <p>Loading...</p> : null}
+      <thead>
+        <tr>
+          <th colSpan="3">Contact List</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Phone</td>
+        </tr>
+        
+        {contacts.map((contact) => {
+          return <ContactRow key={contact.id} contact={contact} />;
+        })}
+      </tbody>
+    </table>
+  );
+}
